8a5ce8d1a217a08bc818789a7a16dbe6181c824b
kwang-ether <kai_wang@brown.edu>
2019-02-21 00:15:26 -0500

    Standardize CLI and timing

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <QCommandLineParser>
 
 #include <iostream>
+#include <chrono>
 
 #include "mesh.h"
 
+using namespace std;
+using namespace std::chrono;
 
 int main(int argc, char *argv[])
 {
@@ -17,26 +20,54 @@ int main(int argc, char *argv[])
     parser.addHelpOption();
     parser.addPositionalArgument("infile", "Input .obj file path");
     parser.addPositionalArgument("outfile", "Output .obj file path");
+    parser.addPositionalArgument("method", "subdivide/simplify/remesh/denoising");
+    parser.addPositionalArgument("args1", "respective argument for the method");
+    parser.addPositionalArgument("args2", "respective argument2 for the method");
+    //TODO: list the options
 
     parser.process(a);
 
     const QStringList args = parser.positionalArguments();
-    if(args.size() != 2) {
-        std::cerr << "Error: Wrong number of arguments" << std::endl;
+    if(args.size() < 4) {
+        cerr << "Error: Wrong number of arguments" << endl;
         a.exit(1);
         return 1;
     }
     QString infile = args[0];
     QString outfile = args[1];
+    QString method = args[2];
+
     ////////////////////////////////////////////////////////////////////////////////
 
     Mesh m;
     m.loadFromFile(infile.toStdString());
 
-    // Here's where you'll do all the heavy-lifting: convert the Mesh m into your
-    //    own mesh data structure for efficient geometry processing, apply some
-    //    operation to that data structure, then convert it back to a basic Mesh.
+    auto t0 = high_resolution_clock::now();
+    // TODO
+    // Convert the mesh into your own data structure
 
+    // TODO
+    // Implement the operations
+    if (method == "subdivide"){
+        //TODO
+    } else if (method == "simplify"){
+        //TODO
+    } else if (method == "remesh"){
+        //TODO
+    } else if (method == "denoising") {
+        //TODO
+    } else {
+        cerr << "Error: Unknown method name" << endl;
+    }
+
+    auto t1 = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(t1-t0).count();
+
+    cout << "Execution takes: " << duration << " milliseconds." <<endl;
+    // TODO
+    // Convert your datastructure back to the basic format
+
+    ////////////////////////////////////////////////////////////////////////////////
     m.saveToFile(outfile.toStdString());
 
     a.exit();
